f2b1d11829134c3545d5aed874cf28eecbbc0bf6
Bryanna-Terrazas <bryanna.terrazas01@utrgv.edu>
2024-10-28 20:22:50 +0000

    saving, again

--- StudentList.cpp
+++ StudentList.cpp
@@ -41,21 +41,59 @@
 
 	//Print out the names of each student in the list.
 	void StudentList::printList() {
-		Node *temp = tail;
-		while(temp != head){
+		Node *temp = head;
+		for(int i = 0; i < numStudents; i++){
+			cout << temp->data.name << " ";
 			temp = temp->next;
 		}
+		temp = nullptr;
+		delete temp;
 	}
 
 	// Remove the Node with the student at the back (tail) of the list
 	// should not fail if list is empty! Print an error message if this occurs
 	// Don't forget that your head and tail pointers will be null pointers if the list is empty
-	void StudentList::popBack() {}
+	void StudentList::popBack() {
+		Node *t = tail;
+		if(head != nullptr){
+			if(head == tail){
+				head = nullptr;
+				tail = nullptr;
+			}
+			else{
+				tail = tail->prev;
+				tail->next = nullptr;
+			}
+			numStudents--;
+		}
+		else{
+			cout << "Error, empty list." << endl;
+
+		}
+
+	}
 
 	// Remove the Node with the student at the front (head) of the list
 	// should not fail if list is empty! Print an error message if this occurs
 	// Don't forget that your head and tail pointers will be null pointers if the list is empty
-	void StudentList::popFront() {}
+	void StudentList::popFront() {
+		Node *t = head;
+		if(head != nullptr){
+		 	if(head == tail){
+				head = nullptr;
+				tail = nullptr;
+			}
+			else{
+				head = head->next;
+				head->prev = nullptr;
+			}
+			numStudents--;
+		}
+		else{
+			cout << "Error, empty stack!"<< endl;
+		}
+
+	}
 
 	//insert a student at the position "index".
 	// for this list, count head as index 0
@@ -63,14 +101,40 @@
 	// print a message and insert the student at the back of the list
 	// the previous Node at your target index should be moved forward. "For exampe, Node with student at index i, becomes index i+1" 
 	// Remember that you already have methods that can add students to the front or back of list if needed! Don't repeat this code.
-	void StudentList::insertStudent(Student s, int index) {}
+	void StudentList::insertStudent(Student s, int index) {
+		Node *t = head;
+		Node *n = new Node(s);
+		if(index > numStudents){
+			cout << "Index does not exist, adding to end of list."<< endl;
+			addBack(s);
+		}
+		else{
+			for(int i = 0; i < index; i++){
+				t = t->next;
+			}
+			t->prev->next = n;
+			n->prev = t->prev->next;
+			t = t->next;
+			n = t->prev;
+			t->prev = n;
+			numStudents++;
+		}
+
+	}
 
 	//find the student with the given id number and return them
 	// if no student matches, print a message 
 	// and create and return a dummy student object
-	Student StudentList::retrieveStudent(int idNum) {
-		Student fixthis;
-		return fixthis;
+	Student StudentList::retrieveStudent(int idNum){
+		
+		Student *n;
+		n = new Student;
+		n->id = idNum;
+		Node *b = new Node(*n);
+		while(b->data.id != idNum){
+			b = b->next;
+		}
+		return b->data;
 	}
 
 	// Remove a Node with a student from the list with a given id number
